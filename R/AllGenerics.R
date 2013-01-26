fa11dd9a00303ed2b1c572d89b6e3c6edfe969cf
Marc Carlson <mcarlson@fhcrc.org>
2013-01-26 00:50:48 +0000

    Adds rBiopaxParser and HTSFilter
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/HTSFilter@72940 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/AllGenerics.R
@@ -0,0 +1,9 @@
+setGeneric(
+		name = "HTSFilter",
+		def = function(x,...) {standardGeneric("HTSFilter")}
+		)
+
+setGeneric(
+		name = "HTSBasicFilter",
+		def = function(x,...) {standardGeneric("HTSBasicFilter")}
+		)
